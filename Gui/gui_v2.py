c5f28f69b49ce5421675a26d81f673a6f3d2a106
zhusiyi <xiaosiyier@163.com>
2021-02-22 23:52:05 +0800

    在成为码农的路上渐行渐远...要改架构

--- Gui/gui_v2.py
+++ Gui/gui_v2.py
@@ -6,6 +6,7 @@ from tkinter.font import BOLD, Font
 
 
 class CentralForm(tk.Toplevel):
+
   def __init__(self, master, my_height=80):
     super().__init__()
     self.master = master
@@ -36,14 +37,16 @@ class AboutPdfSearch(CentralForm):
     self.resizable = False
 
     self.ok_button = Button(self, text="OK", command=self.destroy)
-    self.ok_button.pack(side=tk.BOTTOM,pady=(0,20))
+    self.ok_button.pack(side=tk.BOTTOM, pady=(0, 20))
 
     self.content_text = tk.Text(self,  bg="white", fg="black")
     self.content_text.insert(tk.END, "Developed by ...\nStudents of HUST")
-    self.content_text.pack(side=tk.TOP, fill=tk.X, expand=1,padx=10,pady=5)
+    self.content_text.pack(side=tk.TOP, fill=tk.X,
+                           expand=1, padx=10, pady=5)
     self.content_text.insert("1.0", "PdfSearch 1.0\n")
     self.content_text.tag_add("title", "1.0", "1.13")
-    self.content_text.tag_config("title", font=("Times New Roman", 12, "bold"))
+    self.content_text.tag_config(
+        "title", font=("Times New Roman", 12, "bold"))
     self.content_text.configure(state='disabled')
 
 
@@ -92,6 +95,7 @@ class PdfSearch(tk.Tk):
         self, width=650, height=600, bg="lightgrey")
     self.right_frame.pack_propagate(0)
 
+    # inside left_frame
     self.corpus_label = Label(
         self.left_frame, text="Corpus Files", style="Bold10.TLabel")
     self.corpus_label.pack(side=tk.TOP)
@@ -114,7 +118,119 @@ class PdfSearch(tk.Tk):
     self.total_num_value_label.pack(side=tk.BOTTOM, fill=tk.X)
     self.total_num_label.pack(side=tk.BOTTOM, fill=tk.X)
 
-    self.left_frame.pack(side=tk.LEFT, fill=tk.BOTH)
+    # inside right_frame
+    self.notebook = Notebook(self.right_frame)
+
+    # word_list_tab
+    word_list_tab = tk.Frame(self.notebook, bg="lightgrey")
+    self.word_list_word_types = tk.IntVar(self, value=0)
+    self.word_list_word_tokens = tk.IntVar(self, value=0)
+    self.word_list_search_hits = tk.IntVar(self, value=0)
+    # word_list_top_frame
+    self.word_list_top_frame = tk.Frame(
+        word_list_tab, background="lightgrey")
+    self.word_list_word_types_label = Label(
+        self.word_list_top_frame, text="Word Types: ", style="Bold10.TLabel")
+    self.word_list_word_tokens_label = Label(
+        self.word_list_top_frame, text="Word Tokens: ", style="Bold10.TLabel")
+    self.word_list_search_hits_label = Label(
+        self.word_list_top_frame, text="Search Hits: ", style="Bold10.TLabel")
+    self.word_list_word_types_value_label = Label(
+        self.word_list_top_frame, textvar=str(self.word_list_word_types), background="lightgrey")
+    self.word_list_word_tokens_value_label = Label(
+        self.word_list_top_frame, textvar=str(self.word_list_word_tokens), background="lightgrey")
+    self.word_list_search_hits_value_label = Label(
+        self.word_list_top_frame, textvar=str(self.word_list_search_hits), background="lightgrey")
+    self.word_list_word_types_label.pack(side=tk.LEFT)
+    self.word_list_word_types_value_label.pack(side=tk.LEFT, padx=(5, 20))
+    self.word_list_word_tokens_label.pack(side=tk.LEFT)
+    self.word_list_word_tokens_value_label.pack(side=tk.LEFT, padx=(5, 20))
+    self.word_list_search_hits_label.pack(side=tk.LEFT)
+    self.word_list_search_hits_value_label.pack(side=tk.LEFT, padx=(5, 0))
+
+    self.word_list_top_frame.pack(side=tk.TOP, fill=tk.X, pady=10, padx=10)
+
+    # word_list_bottom_frame
+    self.word_list_bottom_frame = tk.Frame(
+        word_list_tab, background="lightgrey")
+
+
+
+
+    self.search_by_case = tk.IntVar(self, value=0)
+    self.search_by_words = tk.IntVar(self, value=0)
+    self.search_by_regex = tk.IntVar(self, value=0)
+    self.search_term = tk.StringVar(self, value="")
+
+    self.search_term_text = Label(
+        self.word_list_bottom_frame, text="Search Term", style="Bold10.TLabel")
+    self.search_by_words_cb = tk.Checkbutton(
+        self.word_list_bottom_frame, variable=self.search_by_words, text="Words")
+    self.search_by_case_cb = tk.Checkbutton(
+        self.word_list_bottom_frame, variable=self.search_by_case, text="Case")
+    self.search_by_regex_cb = tk.Checkbutton(
+        self.word_list_bottom_frame, variable=self.search_by_regex, text="Regex")
+
+    self.search_term_text.grid(row=1,column=1)
+    self.search_by_words_cb.grid(row=1,column=2)
+    self.search_by_case_cb.grid(row=1,column=3)
+    self.search_by_regex_cb.grid(row=1,column=4)
+
+    self.search_term_entry = tk.Entry(
+        self.word_list_bottom_frame, textvariable=self.search_term)
+    self.search_term_entry.grid(row=2, columnspan=4)
+
+    self.search_start = Button(
+        self.word_list_bottom_frame, text="Start", command=self.start_search)
+    self.search_stop = Button(
+        self.word_list_bottom_frame, text="Stop", command=self.stop_search)
+    self.search_sort = Button(
+        self.word_list_bottom_frame, text="Sort", command=self.sort_search)
+
+    self.search_start.grid(row=3,column=1)
+    self.search_stop.grid(row=3,column=2)
+    self.search_sort.grid(row=3,column=3)
+
+    self.word_list_bottom_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=1)
+
+    # word_list_central_frame
+    self.word_list_central_frame = tk.Frame(
+        word_list_tab, background="lightgrey")
+
+    self.word_list_vertical_scrollbar = tk.Scrollbar(
+        self.word_list_central_frame, orient="vertical", command=self.scroll_rank_and_freq_and_word)
+
+    self.word_list_rank_text = tk.Text(
+        self.word_list_central_frame, width=10)
+    self.word_list_rank_text.configure(state="disabled")
+
+    self.word_list_freq_text = tk.Text(
+        self.word_list_central_frame, width=10)
+    self.word_list_freq_text.configure(state="disabled")
+
+    self.word_list_word_text = tk.Text(
+        self.word_list_central_frame, width=80)
+    self.word_list_word_text.configure(state="disabled")
+
+    self.word_list_freq_text.configure(
+        yscrollcommand=self.word_list_vertical_scrollbar.set)
+    self.word_list_rank_text.configure(
+        yscrollcommand=self.word_list_vertical_scrollbar.set)
+    self.word_list_word_text.configure(
+        yscrollcommand=self.word_list_vertical_scrollbar.set)
+
+    self.word_list_vertical_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    self.word_list_rank_text.pack(side=tk.LEFT, fill=tk.Y, padx=10)
+    self.word_list_freq_text.pack(side=tk.LEFT, fill=tk.Y, padx=10)
+    self.word_list_word_text.pack(side=tk.LEFT, fill=tk.Y, padx=10)
+
+    self.word_list_central_frame.pack(side=tk.TOP, fill=tk.BOTH)
+
+    self.notebook.add(word_list_tab, text="Word List")
+
+    self.notebook.pack(fill=tk.BOTH, expand=1)
+
+    self.left_frame.pack(side=tk.LEFT, fill=tk.BOTH, ipadx=5)
     self.right_frame.pack(side=tk.LEFT, expand=1, fill=tk.BOTH)
 
     self.bind("<Control-f>", self.open_file)
@@ -137,11 +253,40 @@ class PdfSearch(tk.Tk):
   def save_output(self):
     pass
 
+  def start_search(self, event=None):
+    pass
+
+  def stop_search(self, event=None):
+    pass
+
+  def sort_search(self, event=None):
+    pass
+
   def about(self):
     AboutPdfSearch(self)
 
   def view_file(self, event=None):
     pass
+
+  def scroll_rank_and_freq_and_word(self, *args):
+    try:  # from scrollbar
+      self.main_text.yview_moveto(args[1])
+      self.line_numbers.yview_moveto(args[1])
+    except IndexError:
+      # from mouse MouseWheel
+      event = args[0]
+      if event.delta:
+        move = -1*(event.delta/120)
+      else:
+        if event.num == 5:
+          move = 1
+        else:
+          move = -1
+
+      self.main_text.yview_scroll(int(move), "units")
+      self.line_numbers.yview_scroll(int(move), "units")
+
+    return "break"
 
 
 if __name__ == "__main__":
