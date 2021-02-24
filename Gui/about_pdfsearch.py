625d1b4f1ca7e9fdacd2a7e3c2347747d87dbdd2
zhusiyi <xiaosiyier@163.com>
2021-02-24 21:07:00 +0800

    gui拆成很多小文件

---
+++ Gui/about_pdfsearch.py
@@ -0,0 +1,31 @@
+from utils import CentralWindow
+import tkinter as tk
+from tkinter.ttk import Button
+
+class AboutPdfSearch(CentralWindow):
+    def __init__(self, master):
+        my_height = 300
+        super().__init__(master, my_height)
+
+        self.master = master
+
+        self.title("About PdfSearch")
+        self.resizable = False
+
+        self.ok_button = Button(self, text="OK", command=self.destroy)
+        self.ok_button.pack(side=tk.BOTTOM, pady=(0, 20))
+
+        self.bind("<Return>", lambda event=None: self.ok_button.invoke())
+        self.bind("<Escape>", lambda event=None: self.ok_button.invoke())
+
+        self.content_text = tk.Text(self,  bg="white", fg="black")
+        self.content_text.insert(tk.END, "Developed by ...\nStudents of HUST")
+        self.content_text.pack(side=tk.TOP, fill=tk.X,
+                               expand=1, padx=10, pady=5)
+        self.content_text.insert("1.0", "PdfSearch 1.0\n")
+        self.content_text.tag_add("title", "1.0", "1.13")
+        self.content_text.tag_config(
+            "title", font=("Times New Roman", 12, "bold"))
+        self.content_text.configure(state='disabled')
+
+        self.focus_force()
---
+++ Gui/concordance_tab.py
@@ -0,0 +1,19 @@
+import model_tab
+from model_tab import ModelTab
+import tkinter as tk
+
+class ConcordanceTab(ModelTab):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.concordance_hits = tk.IntVar(self, value=0)
+        temp_top = {
+            "Concordance Hits": self.concordance_hits
+        }
+        self.set_top_frame(temp_top)
+        temp_central = {
+            "Hit": 10,
+            "KWIC": 60,
+            "File": 30
+        }
+        self.set_central_frame(temp_central)
